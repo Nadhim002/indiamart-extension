5757772eec98712adc60696fc38dd6df7d5c077d
Levi <levi@example.com>
2026-06-20 18:17:38 +0530

    fix build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   root: './src',
+  plugins: [react()],
   build: {
     outDir: '../dist',
     emptyOutDir: true,
